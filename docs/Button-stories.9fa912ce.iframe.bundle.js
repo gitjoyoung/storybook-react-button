e544f9a9faea283e55178e604c65d7641587e1a8
Jo Young <githuborigin@gmail.com>
2023-07-12 15:17:28 +0900

    깃허브 액션 이슈로 vite -> webpack으로 변경

---
+++ docs/Button-stories.9fa912ce.iframe.bundle.js
@@ -0,0 +1,13 @@
+"use strict";(self.webpackChunkcdd_storybook_wanted_jy=self.webpackChunkcdd_storybook_wanted_jy||[]).push([[463],{"./src/components/Button.stories.js":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,{Default:()=>Default,__namedExportsOrder:()=>__namedExportsOrder,default:()=>Button_stories});__webpack_require__("./node_modules/.pnpm/react@18.2.0/node_modules/react/index.js");var prop_types=__webpack_require__("./node_modules/.pnpm/prop-types@15.8.1/node_modules/prop-types/index.js"),prop_types_default=__webpack_require__.n(prop_types),styled_components_browser_esm=__webpack_require__("./node_modules/.pnpm/styled-components@6.0.2_react-dom@18.2.0_react@18.2.0/node_modules/styled-components/dist/styled-components.browser.esm.js"),jsx_runtime=__webpack_require__("./node_modules/.pnpm/react@18.2.0/node_modules/react/jsx-runtime.js");const Button=({color,backgroundColor,label,...props})=>(0,jsx_runtime.jsx)(DefaultButton,{$color:color,$backgroundColor:backgroundColor,...props,children:label});Button.displayName="Button";const DefaultButton=styled_components_browser_esm.ZP.button`
+  background-color: ${props=>props.$backgroundColor?props.$backgroundColor:"black"};
+  color: ${props=>props.$color?props.$color:"yellow"};
+  font-family: "Nunito Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  font-weight: 700;
+  border: 0;
+  padding: 10px;
+  border-radius: 3em;
+  cursor: pointer;
+  display: inline-block;
+  line-height: 1;
+`;Button.propTypes={backgroundColor:prop_types_default().string,color:prop_types_default().string,label:prop_types_default().string.isRequired,onClick:prop_types_default().func},Button.defaultProps={backgroundColor:null,color:null,onClick:void 0},Button.__docgenInfo={description:"",methods:[],displayName:"Button",props:{backgroundColor:{defaultValue:{value:"null",computed:!1},description:"",type:{name:"string"},required:!1},color:{defaultValue:{value:"null",computed:!1},description:"",type:{name:"string"},required:!1},onClick:{defaultValue:{value:"undefined",computed:!0},description:"",type:{name:"func"},required:!1},label:{description:"",type:{name:"string"},required:!0}}};const Button_stories={title:"Example/Button",component:Button,tags:["autodocs"],argTypes:{backgroundColor:{control:"color"},color:{control:"color"}}},Default={args:{label:"Button"}};Default.parameters={...Default.parameters,docs:{...Default.parameters?.docs,source:{originalSource:"{\n  args: {\n    label: 'Button'\n  }\n}",...Default.parameters?.docs?.source}}};const __namedExportsOrder=["Default"]}}]);
+//# sourceMappingURL=Button-stories.9fa912ce.iframe.bundle.js.map
