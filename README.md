58fdbeb31582113f5a40e6ed0d3be990ebec45d7
Jo Young <githuborigin@gmail.com>
2023-07-12 15:05:52 +0900

    git action을 위해 빌드

--- README.md
+++ README.md
@@ -1,8 +1,22 @@
 ## 원티드 스토리북 예제
 
+버튼 디자인 스토리북 스터디
+
+공부 내용 
+
+### 스토리북을 사용하는 이유?
+
+1 컴포넌트 문서화 (스토리)
+2 협업 팀원들 테스트 용이
+3 재사용성을 고려한 의존성 최소화
+
+
 
 
 ## 배포버전 다운
+
+https://www.npmjs.com/package/cdd-storybook-wanted-jy
+
 install 
 
 npm i cdd-storybook-wanted-jy
---
+++ docs/assets/preview-9f143af8.js
@@ -0,0 +1,2 @@
+import{_ as t}from"./iframe-7bfcae7e.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-EYKKDMVH-355e91f2.js").then(e=>e.D),["./DocsRenderer-EYKKDMVH-355e91f2.js","./iframe-7bfcae7e.js","./index-8db94870.js","./_commonjsHelpers-042e6b4d.js","./_getPrototype-63c5d83d.js","./index-d475d2ea.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
+//# sourceMappingURL=preview-9f143af8.js.map
